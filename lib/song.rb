73a9363b2e10f2a62823dc95d7a4e94c95253b34
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-17 19:04:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,7 +58,13 @@ class Song
  end
  
  def self.new_from_filename(file_name)
-   self.new(file_name).split(" - ")[2]
+   parts = filename.split(" - ")
+   artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
+   
+   artist = Artist.find_or_create_by_name(artist_name)
+   genre = Genre.find_or_create_by_name(genre_name)
+   
+   new(song_name, artist, genre)
    
    
  end
