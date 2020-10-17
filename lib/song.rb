ae92a8166610e49c32019879230b6281d6adc998
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-17 16:58:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,14 @@ class Song
   
   attr_accessor :name
   
-    def initialize(name)
-      @name = name
-      @@all = []
-    end
+  def initialize(name)
+    @name = name
+    @@all = []
+  end
   
+  def self.all
+    @@all
+  end
   
   
   
