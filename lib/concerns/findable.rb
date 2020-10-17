58c82ac276371ca5bcab10838a058a069e852d78
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-17 18:48:48 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,8 +4,9 @@ module Concerns::Findable
    self.all.detect{ |s| s.name == name }
  end
   
-  
-  
+  def find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
   
   
 end
